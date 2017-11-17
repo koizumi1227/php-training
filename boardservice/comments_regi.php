1a5cfb5fdc9b2767a5dc36d0ebc02bfb9b188da5
Shohei Saito <saito.ssh@gmail.com>
2017-11-17 23:01:43 +0900

    Revert "Revert "アポストロフィーの追加""
    This reverts commit e39db87469b430e5c7498e8faa07d29ad70f454a.

--- boardservice/comments_regi.php
+++ boardservice/comments_regi.php
@@ -1,7 +1,7 @@
 <?php
   // 名前未入力の場合「名無しに」変更
-  if($_POST[name] == ''){
-    $_POST[name] = '名無し';
+  if($_POST['name'] == ''){
+    $_POST['name'] = '名無し';
   }
  ?>
 <!DOCTYPE html>
@@ -13,12 +13,12 @@
   <body>
     <!-- コメントの確認と書込かキャンセルの選択画面 -->
   <form action="comments.php" method="post">
-    <p>名前 : <?php echo $_POST[name]?></p>
-    <p>タイトル : <?php echo $_POST[title]?></p>
-    <p>コメント内容 : <?php echo $_POST[text]?></p>
-    <input type="hidden" name="name" value="<?php echo $_POST[name] ?>">
-    <input type="hidden" name="title" value="<?php echo $_POST[title]?>">
-    <input type="hidden" name="text" value="<?php echo $_POST[text]?>">
+    <p>名前 : <?php echo $_POST['name']?></p>
+    <p>タイトル : <?php echo $_POST['title']?></p>
+    <p>コメント内容 : <?php echo $_POST['text']?></p>
+    <input type="hidden" name="name" value="<?php echo $_POST['name'] ?>">
+    <input type="hidden" name="title" value="<?php echo $_POST['title']?>">
+    <input type="hidden" name="text" value="<?php echo $_POST['text']?>">
     <br>
     <p>上記を書き込みます</p>
       <button type="submit" name="action" value="accept">書込</button>
