fef3f6ac4a141a91c000575bfc626ee8df73418a
YK-PC\YK <fax1227@hotmail.co.jp>
2017-11-20 17:56:45 +0900

    html出力する場所にエスケープ処理追加

--- boardservice/comments_regi.php
+++ boardservice/comments_regi.php
@@ -11,14 +11,15 @@
     <title>コメント確認</title>
   </head>
   <body>
+  <?php require_once 'function.php'; ?>
     <!-- コメントの確認と書込かキャンセルの選択画面 -->
   <form action="comments.php" method="post">
-    <p>名前 : <?php echo $_POST['name']?></p>
-    <p>タイトル : <?php echo $_POST['title']?></p>
-    <p>コメント内容 : <?php echo $_POST['text']?></p>
-    <input type="hidden" name="name" value="<?php echo $_POST['name'] ?>">
-    <input type="hidden" name="title" value="<?php echo $_POST['title']?>">
-    <input type="hidden" name="text" value="<?php echo $_POST['text']?>">
+    <p>名前 : <?php echo e($_POST['name'])?></p>
+    <p>タイトル : <?php echo e($_POST['title'])?></p>
+    <p>コメント内容 : <?php echo e($_POST['text'])?></p>
+    <input type="hidden" name="name" value="<?php echo e($_POST['name']) ?>">
+    <input type="hidden" name="title" value="<?php echo e($_POST['title'])?>">
+    <input type="hidden" name="text" value="<?php echo e($_POST['text'])?>">
     <br>
     <p>上記を書き込みます</p>
       <button type="submit" name="action" value="accept">書込</button>
