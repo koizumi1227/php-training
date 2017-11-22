7af47a85bd5abdc1bb28c1f6fe17752989cf20b8
YK-PC\YK <fax1227@hotmail.co.jp>
2017-11-22 15:03:38 +0900

    コメント一覧から削除を選んだ際の確認ファイル（コメント内容がでる

---
+++ boardservice/delete_regi.php
@@ -0,0 +1,44 @@
+<?php
+  $id = $_GET['id'];
+  require_once 'function.php';
+  require_once 'db_connect.php';
+try{
+  $dbh = db_connect();
+  $dbh -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  date_default_timezone_set('Asia/Tokyo');
+
+  $sql = 'SELECT * FROM comments WHERE id = :id';
+  $pre = $dbh->prepare($sql);
+  $pre->bindValue(':id', $id);
+  $r = $pre->execute();
+} catch (PDOException $e) {
+  echo "エラーが発生。最初から再度IDを選択。 (" , $e->getMessage() , ")";
+  return ;
+
+}
+ ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>コメント詳細</title>
+  </head>
+  <body>
+    <?php $data = $pre->fetch(PDO::FETCH_ASSOC);?>
+    <p>ID : <?php echo h($data['id']) ?></p>
+    <p>名前 : <?php echo h($data['name']) ?></p>
+    <p>タイトル : <?php echo h($data['title']) ?></p>
+    <p>コメント内容:</p>
+    <pre>
+      <?php echo h($data['text']) ?>
+    </pre>
+    <br>
+
+    <form action="delete.php" method="POST">
+      <input type="hidden" name="id" value="<?php echo h($data['id'])?>">
+      <button type="submit" name="action" value="accept">削除</button>
+      <button type="submit" name="action" value="cancel">キャンセル</button>
+    </form>
+    </body>
+  </html>
