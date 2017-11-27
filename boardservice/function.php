089b829c8eb8639897aa33ea36027d32ba9bcb17
YK-PC\YK <fax1227@hotmail.co.jp>
2017-11-27 14:19:49 +0900

    CSRF用のtokenを追加（）

--- boardservice/function.php
+++ boardservice/function.php
@@ -1,5 +1,17 @@
 <?php
-function h($str) {
-    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+  function h($str) {
+      return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+    }
+
+  // CSRFトークンの生成
+  function generate_token() {
+      // セッションIDからハッシュを生成
+      return hash ( 'sha256', session_id() );
   }
+
+  // CSRFトークン
+  function validate_token ($token) {
+      return $token === generate_token();
+  }
+
 ?>
