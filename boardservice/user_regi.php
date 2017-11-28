6397fa3e4166eff5618dc6c7e0b60b0e03d61d6c
YK-PC\YK <fax1227@hotmail.co.jp>
2017-11-28 17:20:13 +0900

    passとemailのhtml表示を追加。未入力がどうかのチェックを追加。

--- boardservice/user_regi.php
+++ boardservice/user_regi.php
@@ -1,3 +1,22 @@
+<?php
+ require_once 'function.php';
+
+ // 未入力チェック
+   if (empty($_POST["name"])) {  // 値が空のとき
+       $errorMessage = '名前が未入力です。';
+   } else if (empty($_POST["mail"])) {
+       $errorMessage = 'メールアドレスが未入力です。';
+   } else if (empty($_POST["password"])) {
+       $errorMessage = 'パスワードが未入力です。';
+   }
+ if (empty($_POST["name"]) || empty($_POST["mail"]) || empty($_POST["password"])){
+   echo $errorMessage."<br>";
+   echo "<a href='user_regi_form.php'>こちら</a>から再度登録をお願いします。";
+   exit;
+ }
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -5,14 +24,14 @@
     <title>ユーザー登録確認</title>
   </head>
   <body>
-  <?php require_once 'function.php'; ?>
   <form action="user.php" method="post">
     <h1>登録内容確認</h2>
-    <p>名前 : <?php echo h($_POST['name'])?></p>
+      <p>名前 : <?php echo h($_POST['name'])?></p>
+      <p>メールアドレス : <?php echo h($_POST['mail'])?></p>
+      <p>パスワードは表示されません</p>
     <input type="hidden" name="name" value="<?php echo h($_POST['name']) ?>">
-    <!--  パスワードとメールアドレスを追加した場合-->
-    <!-- h($_POST['mail']) -->
-    <!-- h($_POST['password']) -->
+    <input type="hidden" name="mail" value="<?php echo h($_POST['mail']) ?>">
+    <input type="hidden" name="password" value="<?php echo h($_POST['password']) ?>">
     <br>
       <button type="submit" name="action" value="accept">登録</button>
       <button type="submit" name="action" value="cancel">キャンセル</button>
