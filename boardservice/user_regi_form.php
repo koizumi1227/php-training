67cebe3e0e031867423c9a1f35a5f6ca96327680
YK-PC\YK <fax1227@hotmail.co.jp>
2017-11-28 17:11:39 +0900

    email.passの登録フォーム追加。ログイン済の状態ならコメント一覧へ戻るを追加。

--- boardservice/user_regi_form.php
+++ boardservice/user_regi_form.php
@@ -1,3 +1,15 @@
+<?php
+  require_once 'function.php';
+
+  session_start();
+  if (isset($_SESSION['name'])) {
+    echo $_SESSION['name']."でログインされています<br>";
+    echo "<a href=board_service.php>コメント一覧へ戻る</a>";
+    exit;
+  }
+
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,9 +20,8 @@
     <h1>ユーザー登録</h1>
     <form action="user_regi.php" method="post">
       <p>名前:<input type="name" name="name" size="30" placeholder="名前"></p>
-      <!-- パスワードとメールアドレスを追加した場合 -->
-      <!-- <p>メールアドレス:<input type="text" name="mail" size="30" placeholder="example@example.com"></p> -->
-      <!-- <p>パスワード:<input type="password" name="password" size="10" placeholder="パスワード"></p> -->
+      <p>メールアドレス:<input type="text" name="mail" size="30" placeholder="example@example.com"></p>
+      <p>パスワード:<input type="password" name="password" size="10" placeholder="パスワード"></p>
       <input type="submit" value="登録">
 
     </form>
