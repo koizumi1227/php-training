ef3c996b35b5a7fe8825d83a35cdad10ccf77f22
YK-PC\YK <fax1227@hotmail.co.jp>
2017-12-01 19:31:20 +0900

    ファイル名変更

--- boardservice/comment_form.php
+++ boardservice/comment_form.php
@@ -1,3 +1,10 @@
+<?php
+  require_once 'function.php';
+  session_start();
+    //未ログイン時にログイン、新規登録、コメント一覧へ誘導
+  unlogined_session();
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -7,7 +14,7 @@
   <body>
     <!-- コメント入力画面 -->
     <form action="comments_regi.php" method="POST">
-      <p>名前:<input type="name" name="name" size="30" placeholder="省略可（名無し）"></p>
+      <p>名前: <?php echo h($_SESSION['name']) ?></p>
       <p>タイトル:<input type="title" name="title" size="30" placeholder="タイトル"></p>
       <p><textarea name="text" rows="4" cols="40" placeholder="コメント"></textarea></p>
       <input type="submit" value="コメ確認">
