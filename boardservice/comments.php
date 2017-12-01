a7f121aaf2018adf2959d7440b851d69860fba5e
YK-PC\YK <fax1227@hotmail.co.jp>
2017-12-01 19:36:51 +0900

    commentsテーブルからnameを削除したためSQLを変更。sessionからID取得、それをcommentsテーブルへ保存（ユーザー判別のため）。

--- boardservice/comments.php
+++ boardservice/comments.php
@@ -1,43 +1,49 @@
 <?php
-// comment_regi.phpからコメントをDBへ保存
-if ($_POST['action'] == 'cancel') {
-  header('Location: board_service.php');
-exit;
-}
+  require_once 'db_connect.php';
 
-require_once 'db_connect.php';
-try{
-    $dbh = db_connect();
-    date_default_timezone_set('Asia/Tokyo');
+  // comment_regi.phpからコメントをDBへ保存
+  if ($_POST['action'] == 'cancel') {
+    header('Location: index.php');
+  exit;
+  }
+
+  session_start();
+
+  try{
+      $dbh = db_connect();
+      date_default_timezone_set('Asia/Tokyo');
+
+      // $_SESSION[id]はcomments.user_id と同じ
+      // comments.user_id と users.id は同じ
+      // コメント内容
+      $title = $_POST['title'];
+      $text = $_POST['text'];
+      $created_at = date('Y-m-d H:i:s');
+      $user_id = $_SESSION['id'];
+      // var_dump($_SESSION);
 
 
-    // コメント内容
-    $name = $_POST['name'];
-    $title = $_POST['title'];
-    $text = $_POST['text'];
-    $created_at = date('Y-m-d H:i:s');
+      $sql = 'INSERT INTO comments(title,created_at,text,user_id) VALUES(:title,:created_at,:text,:user_id)';
+      $pre = $dbh->prepare($sql);
 
-    $sql = 'INSERT INTO comments(name,title,created_at,text) VALUES(:name,:title,:created_at,:text)';
-    $pre = $dbh->prepare($sql);
+      $pre->bindValue(':title', $title, PDO::PARAM_STR);
+      $pre->bindValue(':text', $text, PDO::PARAM_STR);
+      $pre->bindValue(':created_at', $created_at, PDO::PARAM_INT);
+      $pre->bindValue(':user_id', $user_id, PDO::PARAM_STR);
 
-    $pre->bindValue(':name', $name, PDO::PARAM_STR);
-    $pre->bindValue(':title', $title, PDO::PARAM_STR);
-    $pre->bindValue(':text', $text, PDO::PARAM_STR);
-    $pre->bindValue(':created_at', $created_at, PDO::PARAM_INT);
+      $r = $pre->execute();
+  } catch (PDOException $e) {
+      echo "コメント書き込みに失敗しました。 (" , $e->getMessage() , ")";
+      return ;
 
-    $r = $pre->execute();
-} catch (PDOException $e) {
-    echo "コメント書き込みに失敗しました。 (" , $e->getMessage() , ")";
-    return ;
-
-}
+  }
 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="refresh" content="1;URL=board_service.php">
+    <meta http-equiv="refresh" content="1;URL=index.php">
     <title>コメント完了</title>
   </head>
   <body>
