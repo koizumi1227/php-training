442137404d636903d4daa8efdcced3b155805ca7
YK-PC\YK <fax1227@hotmail.co.jp>
2017-12-01 19:37:16 +0900

    ログイン認証追加のためユーザー名のみの判別を削除

--- boardservice/comments_regi.php
+++ boardservice/comments_regi.php
@@ -1,32 +1,6 @@
 <?php
   require_once 'function.php';
-  require_once 'db_connect.php';
-
-  // ユーザー名一致判別
-  try{
-      $dbh = db_connect();
-      $dbh -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-      $names = $_POST['name'];
-      $sql = 'SELECT * FROM users WHERE user_name = :user_name';
-      $pre = $dbh -> prepare($sql);
-
-      $pre->bindValue(':user_name', $names, PDO::PARAM_STR);
-      $r = $pre->execute();
-      // user_nameに該当するものがあるかを数える。なければ０。
-      $count = $pre->rowCount();
-
-      if($count == 0){
-        echo "該当するユーザー名がありません<br>";
-        echo "<a href=user_regi_form.php>こちら</a>からユーザー登録をしてください<br>";
-        exit;
-      }
-
-  } catch (PDOException $e) {
-      echo "エラーが発生。再度始めからやり直してください。 (" , $e->getMessage() , ")";
-      return ;
-
-  }
+  session_start();
 ?>
 
 <!DOCTYPE html>
@@ -36,10 +10,9 @@
     <title>コメント確認</title>
   </head>
   <body>
-  <?php require_once 'function.php'; ?>
     <!-- コメントの確認と書込かキャンセルの選択画面 -->
   <form action="comments.php" method="post">
-    <p>名前 : <?php echo h($_POST['name'])?></p>
+    <p>名前 : <?php echo h($_SESSION['name'])?></p>
     <p>タイトル : <?php echo h($_POST['title'])?></p>
     <p>コメント内容 : <?php echo h($_POST['text'])?></p>
     <input type="hidden" name="name" value="<?php echo h($_POST['name']) ?>">
