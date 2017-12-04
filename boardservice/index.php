83797d55117e0b976e37993f1bf8c2ad60bbb740
YK-PC\YK <fax1227@hotmail.co.jp>
2017-12-04 22:32:44 +0900

    スレッド一覧のファイルをindex.phpへ変更

--- boardservice/index.php
+++ boardservice/index.php
@@ -1,60 +1,52 @@
 <?php
+
   require_once 'function.php';
   require_once 'db_connect.php';
   session_start();
   // var_dump($_SESSION);
 
-  try {
+try {
 
-    $dbh = db_connect();
-    $sql = 'SELECT comments.*, users.name FROM users INNER JOIN comments ON comments.user_id = users.id';
-    // $sql = 'SELECT * FROM comments';
-    $pre = $dbh -> prepare($sql);
-    $r = $pre->execute();
+  $dbh = db_connect();
+  $sql = 'SELECT * FROM threads';
+  $pre = $dbh -> prepare($sql);
+  $r = $pre->execute();
 
-  } catch (PDOException $e) {
-      echo "エラーが発生。再度始めからやり直してください。 (" , $e->getMessage() , ")";
-      return ;
-  }
 
- ?>
+} catch (PDOException $e) {
+    echo "エラーが発生。再度始めからやり直してください。 (" , $e->getMessage() , ")";
+    return ;
+}
+
+  ?>
 
 <!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="utf-8">
-    <title>コメント一覧</title>
-    <link rel="stylesheet" type="text/css" href="./css/index.css">
-
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <title>スレッド一覧</title>
   </head>
   <body>
-    <h1>コメント一覧</h1>
+    <h1>スレッド一覧</h1>
     <table border="1">
       <tr>
-        <th>ID</th>
-        <th>名前</th>
+        <th>スレッド名</th>
         <th>作成日時</th>
-        <th>変更日時</th>
-        <th>タイトル</th>
-        <th>コメント内容</th>
       </tr>
-      <?php
-        while($data = $pre->fetch(PDO::FETCH_ASSOC)){
+        <?php
+          while($data = $pre->fetch(PDO::FETCH_ASSOC)){
           // echo"<pre>";
           // var_dump($data);
-          // h関数(htmlspecialchars)
-
         ?>
-        <tr>
-          <td><?php echo h($data['id'])?></td>
-          <td><?php echo h($data['name']) ?></td>
-          <td><?php echo h($data['created_at']) ?></td>
-          <td><?php echo h($data['updated_at']) ?></td>
-          <td><?php echo h($data['title']) ?></td>
-          <td><?php echo h($data['text']) ?></td>
-        </tr>
-      <?php } ?>
+          <tr>
+            <td><?php echo "<a href='thread_comment.php?id={$data["id"]}&title={$data["title"]}'>".h($data['title'])."</a>"?>
+            </td>
+            <td><?php echo h($data['created_at']) ?></td>
+          </tr>
 
+        <?php
+          } ?>
     </table>
     <div class="form_conf">
       <a href="login.php">ログイン</a>
@@ -62,6 +54,5 @@
       <a href="comment_form.php">コメント投稿</a>
       <a href="user_comment_list.php">自分のコメント表示</a>
     </div>
-
   </body>
 </html>
